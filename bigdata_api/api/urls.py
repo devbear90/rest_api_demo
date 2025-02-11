fca79ec01515c5ffe108f7dafda88edba1b7b432
Kovács Richárd <kovacs.richard90@gmail.com>
2025-02-11 21:44:33 +0100

    add postman examples

--- bigdata_api/api/urls.py
+++ bigdata_api/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LargeDatasetViewSet
+from .views import LargeDatasetViewSet, LongRunningView
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.authtoken.views import obtain_auth_token
@@ -12,5 +12,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token-auth/", obtain_auth_token, name="api_token_auth")
+    path("token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("long-process/", LongRunningView.as_view(), name="long_process"),
 ]
--- bigdata_api/api/views.py
+++ bigdata_api/api/views.py
@@ -1,3 +1,8 @@
+import time
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import LargeDataset
@@ -7,3 +12,17 @@ class LargeDatasetViewSet(viewsets.ModelViewSet):
     queryset = LargeDataset.objects.all()
     serializer_class = LargeDatasetSerializer
     permission_classes = [IsAuthenticated]
+
+class LongRunningView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        start_time = time.time()
+        
+        # 6 perc várakozás
+        time.sleep(360)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        return Response({"message": "Process completed", "elapsed_time": elapsed_time})
