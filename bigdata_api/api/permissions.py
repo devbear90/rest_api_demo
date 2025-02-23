0e0a3a01a2c5bd786ac8ca5ca2c4273dcef5290f
Kovács Richárd <kovacs.richard90@gmail.com>
2025-02-23 19:42:34 +0100

    add comment

--- bigdata_api/api/permissions.py
+++ bigdata_api/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+# object level custom permission example
 class IsPostAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
